93ec6ab5074b89f16acbdaad295bb319da37f2f5
Rick <rick.eihara@gmail.com>
2016-10-30 23:24:21 -0200

    AssignmentStatement ast

---
+++ 1/comp/src/ast/AssignmentStatement.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ast;
+
+/**
+ *
+ * @author floss
+ */
+public class AssignmentStatement extends Statement{
+	
+	private AssignmentExpr assignExpr;
+	public AssignmentStatement(AssignmentExpr assignExpr){
+		this.assignExpr = assignExpr;
+	}
+
+	public AssignmentExpr getAssignExpr() {
+		return assignExpr;
+	}
+	
+	@Override
+	public void genC(PW pw) {
+	}
+	
+}
