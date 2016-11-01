b83bf5b7b0460512b3058263d95efbfa9c7a7ab3
Rick <rick.eihara@gmail.com>
2016-11-01 02:17:56 -0200

    Forget to add this classes

---
+++ 1/comp/src/ast/FactorNew.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ast;
+
+/**
+ *
+ * @author floss
+ */
+public class FactorNew extends Expr{
+	public FactorNew(KraClass kraclass){
+		this.kraclass = kraclass;
+	}
+	
+	private KraClass kraclass;
+
+	public KraClass getKraclass() {
+		return kraclass;
+	}
+	
+	@Override
+	public void genC(PW pw, boolean putParenthesis) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public Type getType() {
+		return kraclass;
+	}
+	
+}
